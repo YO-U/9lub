285626748ae59ee78e95605aec4a8bbffc66ca05
enoxy <enoxy@DESKTOP-4BU1KFC>
2022-06-07 21:27:14 +0200

    Добавьте файлы проекта.

---
+++ 9lub/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите двоичное число до запятой");
+            var per = Console.ReadLine();
+            Console.WriteLine("Введите двоичное число после запятой");
+            var vtor = Console.ReadLine();
+            try
+            {
+                long nach1 = Convert.ToInt32(per, 2);
+                long nach2 = Convert.ToInt32(vtor, 2);
+                string res1 = Convert.ToString(nach1, 10);
+                string res2 = Convert.ToString(nach2, 10);
+                Console.WriteLine(res1 + "." + res2);               
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
